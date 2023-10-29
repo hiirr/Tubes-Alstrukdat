654d60d3e830c1113f9386f7ed54ac7d42e36b01
RealAzzmi <azmibazeid@gmail.com>
2023-10-29 14:51:49 +0700

    Clean Datetime

--- ADT/Datetime.c
+++ ADT/Datetime.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 char *current_time() {
-    time_t rawtime;
-    struct tm *timeinfo;
+    time_t raw_time;
+    struct tm *time_info;
 
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    return asctime(timeinfo);
+    time(&raw_time);
+    time_info = localtime(&raw_time);
+    return asctime(time_info);
 }
