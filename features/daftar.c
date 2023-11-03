5967a608e4aaff5897cde2cc80d562494c7e7e2e
randyver <randyverdian99@gmail.com>
2023-11-03 16:06:10 +0700

    feat: add registered user to database

--- features/daftar.c
+++ features/daftar.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include "..\ADT\Wordmachine.h"
+#include "../ADT/Wordmachine.h"
 #include "../database/database.h"
 
 void daftar(){
+    char *name, *password;
     if (current_user != -1){
         printf("Anda sudah masuk. Keluar terlebih dahulu untuk melakukan daftar.\n");
     }
     else{
         boolean is_valid_name = true;
         do{
+            is_valid_name = true;
             printf("Masukkan nama:\n");
             START();
             get_paragraph();
-            char *name = input_to_string();
+            name = input_to_string();
             for(int i = 0; i < 20; i++){
                 if (is_two_string_equal(name, users[i].name)){
                     is_valid_name = false;
@@ -27,7 +29,10 @@ void daftar(){
         printf("Masukkan kata sandi:\n");
         START();
         get_paragraph();
-        char *password = input_to_string();
+        password = input_to_string();
+        set_user_name(&users[total_user], name);
+        set_user_password(&users[total_user], password);
+        total_user += 1;
         printf("Pengguna telah berhasil terdaftar. Masuk untuk menikmati fitur-fitur BurBir.\n");
     }
 }
