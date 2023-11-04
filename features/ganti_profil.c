085f1b378983234a5b150de49090de5ee5b85101
hiirr <124429888+hiirr@users.noreply.github.com>
2023-11-04 11:16:49 +0700

    Update ganti_profil.c

--- features/ganti_profil.c
+++ features/ganti_profil.c
@@ -1,29 +1,34 @@
-#include "ADT/boolean.h"
-#include "ADT/Wordmachine.h"
-#include "assets/User.h"
-#include "database/database.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "ganti_profil.h"
 
-User user[MAX_USER];
+#include "../ADT/boolean.h"
+#include "../ADT/Wordmachine.h"
 
-void ganti_profil(int id_user){
+#include "../assets/User.h"
+#include "../database/database.h"
 
-    printf("| Nama: %c\n", user[id_user].name);
-    printf("| Bio Akun: %c\n", user[id_user].bio);
-    printf("| No HP: %c\n", user[id_user].phone);
-    printf("| Weton: %c\n", user[id_user].weton);
+void ganti_profil(){
+
+    printf("| Nama: %c\n", users[current_user].name);
+    printf("| Bio Akun: %c\n", users[current_user].bio);
+    printf("| No HP: %c\n", users[current_user].phone);
+    printf("| Weton: %c\n", users[current_user].weton);
 
     printf("Masukkan Bio Akun: \n");
+    get_paragraph();
     char *new_bio = input_to_string();
     if (!is_current_word_equal("")){
-        set_user_bio(&user[id_user], new_bio);
+        set_user_bio(&users[current_user], new_bio);
     }
 
     printf("Masukkan No HP: \n");
+    get_word();
     char *new_phone = input_to_string();
     boolean valid = false;
     while (!is_current_word_equal("") && !valid){
         if (is_all_int(new_phone)){
-            set_user_bio(&user[id_user], new_phone);
+            set_user_phone(&users[current_user], new_phone);
             valid = true;
         } else {
             printf("Nomor HP anda tidak valid, coba ulangi masukan!");
@@ -32,15 +37,19 @@ void ganti_profil(int id_user){
     } 
 
     printf("Masukkan Weton: \n");
-    char *new_bio = input_to_string();
+    get_word();
+    char *new_weton = input_to_string();
     boolean valid = false;
     while (!is_current_word_equal("") && !valid){
-        if (is_current_word_equal("Pahing") || is_current_word_equal("pahing") || is_current_word_equal("Kliwon") || is_current_word_equal("kliwon") || is_current_word_equal("Wage") || is_current_word_equal("wage") || is_current_word_equal("Pon") || is_current_word_equal("pon") || is_current_word_equal("Legi") || is_current_word_equal("legi")){
-            set_user_bio(&user[id_user], new_bio);
+        for (int i = 0; i < current_word.length; ++i) {
+            new_weton[i] = to_lower(new_weton[i]);
+        }
+        if (is_two_string_equal("pahing", &new_weton) || is_two_string_equal("kliwon", &new_weton) || is_two_string_equal("wage", &new_weton) || is_two_string_equal("pon", &new_weton) || is_two_string_equal("legi", &new_weton)){
+            set_user_weton(&users[current_user], new_weton);
             valid = true;
         } else {
             printf("Weton anda tidak valid, coba ulangi masukan!");
-            *new_bio = input_to_string();
+            *new_weton = input_to_string();
         }
     } 
 }
