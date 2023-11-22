3f3407cfa240f5d6730e3c1ea2dc57d600a2bc34
salsbiila <salsabiilabslmh@gmail.com>
2023-11-22 23:59:29 +0700

    style: modified printed message

--- features/ubah_kicauan.c
+++ features/ubah_kicauan.c
@@ -5,7 +5,7 @@
 
 void ubah_kicauan(int tweet_id) {
     if (current_user == -1) {
-        printf("Anda belum login. Silakan login terlebih dahulu.\n");
+        printf("\nAnda belum masuk! Masuk terlebih dahulu untuk menikmati layanan BurBir.\n\n");
         return;
     }
     if (tweet_id >= latest_tweet) {
